01eb73d1c746153b98f576dde7b5292a4e9fc231
Jehg13 <115719170+Jehg13@users.noreply.github.com>
2022-10-13 09:01:37 -0500

    Add files via upload

---
+++ JesusH/echoandprint.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h2>PHP is Fun</h2>";
+echo "Hello World! <br>";
+echo "I´m about to learn PGP <br>";
+echo "This", "String", "was", "made", "with multiple parameters";
+?>
+
+<?php
+print "<h2>PHP is Fun</h2>";
+print "Hello World! <br>";
+print "I´m about to learn php <br>";
+?>
---
+++ JesusH/variables.php
@@ -0,0 +1,16 @@
+<?php
+// Ejemplos de nombres de variables
+$edad = 16;
+$edad1 = 17;
+$edad_1 = 18;
+$age= 3.1515;
+// Ejemplos de nombres de variables (estilos NO recomendados)
+$años = 19;
+$_edad = 20;
+$Edad = 21;
+$__edad = 22;
+$EDAD = 23;
+$_EDAD = 24;
+
+var_dump($age);
+?>
